52e9675cfda0269368faca65f434c86c5d43649e
NarcisN <narcisn650@gmail.com>
2024-10-29 10:38:49 +0200

    final

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>ss</div>;
+};
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "c3622f6543mshe364417d9aef2c0p17f03ejsn8da1d3f55445",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "c3622f6543mshe364417d9aef2c0p17f03ejsn8da1d3f55445",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
